8d6886de124ccbc8803f2d5528083b9715b4019f
shendrie22 <sherlinedeguzman3@gmail.com>
2025-05-26 22:03:36 +0800

    updated icons for sidebars

--- lvconnect-frontend/src/config/roleMenus.jsx
+++ lvconnect-frontend/src/config/roleMenus.jsx
@@ -1,38 +1,49 @@
-import { HiHome, HiOutlineHome, HiClipboardDocumentList, HiOutlineClipboardDocumentList } from "react-icons/hi2";
-import { FaAddressCard, FaRegAddressCard, FaFileAlt, FaRegFileAlt } from "react-icons/fa";
-import { RiGraduationCapFill, RiGraduationCapLine } from "react-icons/ri";
+import { HiHome, HiOutlineHome, HiClipboardDocumentList, HiOutlineClipboardDocumentList, HiOutlineUsers, HiUsers, HiMiniShieldCheck} from "react-icons/hi2";
+import { FaAddressCard, FaRegAddressCard, FaFileAlt, FaRegFileAlt, } from "react-icons/fa";
+import { RiGraduationCapFill, RiGraduationCapLine, RiArchiveFill, RiFileCopy2Fill, RiFileCopy2Line } from "react-icons/ri";
+import { IoNewspaperOutline, IoNewspaper} from "react-icons/io5";
+import { GoArchive } from "react-icons/go";
+import { MdOutlineFileCopy, MdFileCopy} from "react-icons/md";
+import { BiSolidFolderOpen, BiFolderOpen } from "react-icons/bi";
+import { HiOutlineShieldCheck } from "react-icons/hi"
 
 export const roleMenus = {
   student: [
-    { name: "Dashboard", path: "/my", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-    { name: "Enrollment", path: "/my/enrollment", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-    { name: "Grades", path: "/my/grades", solidIcon: FaFileAlt, outlineIcon: FaRegFileAlt },
-    { name: "SOA", path: "/my/soa", solidIcon: HiClipboardDocumentList, outlineIcon: HiOutlineClipboardDocumentList },
-    { name: "Student Services", path: "/my/student-services", solidIcon: RiGraduationCapFill, outlineIcon: RiGraduationCapLine },
-    { name: "Surveys", path: "/my/surveys", solidIcon: RiGraduationCapFill, outlineIcon: RiGraduationCapLine },
-  ],
-  comms: [
-    { name: "Dashboard", path: "/comms-admin", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-    { name: "Updates", path: "/comms-admin/posts", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-    { name: "Archive", path: "/comms-admin/archive", solidIcon: FaFileAlt, outlineIcon: FaRegFileAlt },
-  ],
-  scadmin: [
-    { name: "Dashboard", path: "/school-admin", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-    { name: "Review Posts", path: "/school-admin/posts/review", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-  ],
-  psas: [
-    { name: "Dashboard", path: "/psas-admin", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-    { name: "Survey", path: "/psas-admin/surveys",match: "/psas-admin/surveys", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-    { name: "School Forms", path: "/psas-admin/forms", solidIcon: FaFileAlt, outlineIcon: FaRegFileAlt },
-  ],
-  registrar: [
-    { name: "Dashboard", path: "/registrar", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-    { name: "Enrollment", path: "/registrar/enrollment", match: "/registrar/enrollment", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-  
-    { name: "Students", path: "/registrar/student-information-management", solidIcon: FaFileAlt, outlineIcon: FaRegFileAlt },
-    { name: "SOA", path: "/registrar/soa", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
-  ],
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "Enrollment", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
+     { name: "Grades", solidIcon: FaFileAlt, outlineIcon: FaRegFileAlt },
+     { name: "SOA", solidIcon: HiClipboardDocumentList, outlineIcon: HiOutlineClipboardDocumentList },
+     { name: "Student Services", solidIcon: RiGraduationCapFill, outlineIcon: RiGraduationCapLine },
+     { name: "Surveys", solidIcon: MdFileCopy, outlineIcon: MdOutlineFileCopy },
+   ],
+   comms: [
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "Updates", solidIcon: IoNewspaper, outlineIcon: IoNewspaperOutline },
+     { name: "Archive", solidIcon: RiArchiveFill, outlineIcon: GoArchive },
+   ],
+   scadmin: [
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "School Updates", solidIcon: RiFileCopy2Fill, outlineIcon: RiFileCopy2Line },
+   ],
+   psas: [
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "Survey", solidIcon: MdFileCopy, outlineIcon: MdOutlineFileCopy },
+     { name: "School Forms", solidIcon: BiSolidFolderOpen, outlineIcon: BiFolderOpen },
+   ],
+   registrar: [
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "Enrollment", solidIcon: FaAddressCard, outlineIcon: FaRegAddressCard },
+     { name: "Students", solidIcon: HiUsers, outlineIcon: HiOutlineUsers },
+     { name: "SOA", solidIcon: HiClipboardDocumentList, outlineIcon: HiOutlineClipboardDocumentList },
+     { name: "Archive", solidIcon: RiArchiveFill, outlineIcon: GoArchive },
+   ],
    superadmin: [
-    { name: "Dashboard", path: "/system-admin", solidIcon: HiHome, outlineIcon: HiOutlineHome },
-  ],
-};
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+   ],
+    systemadmin: [
+     { name: "Dashboard", solidIcon: HiHome, outlineIcon: HiOutlineHome },
+     { name: "Users", solidIcon: HiUsers, outlineIcon: HiOutlineUsers },
+     { name: "Roles", solidIcon: HiMiniShieldCheck, outlineIcon: HiOutlineShieldCheck },
+     { name: "Archive", solidIcon: RiArchiveFill, outlineIcon: GoArchive },
+   ],
+ };
