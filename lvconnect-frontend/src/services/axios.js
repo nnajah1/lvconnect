743ab05e785b7c2965b363770c87c9fa2c185679
nnajah1 <jannahdelarosa65@gmail.com>
2025-05-27 16:43:58 +0800

    add ENV

--- lvconnect-frontend/src/services/axios.js
+++ lvconnect-frontend/src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // export const baseURL = "http://127.0.0.1:8000/api";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // ⬅ Ensures cookies are sent with requests
 
 });
